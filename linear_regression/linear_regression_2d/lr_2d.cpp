e6eea8fe0d26ee763e04c6f381a542c969edaba3
Varun Sundar <varunsundar08@gmail.com>
2016-06-13 19:44:07 +0530

    Add 2d linear regression code

---
+++ linear_regression/linear_regression_2d/lr_2d.cpp
@@ -0,0 +1,68 @@
+
+/* Defines functions in lr_2d.hpp */
+
+#include "lr_2d.hpp";
+#include <assert>
+
+float cost_function_m_slope(vector<float> input_x,
+                            vector<float> input_y,
+                            float m,
+                            float c) {
+    assert(input_x.size() == input_y.size());
+
+    /*
+     * Note : n is the number of input points
+     * Cost function : [ (1/2n) * summation over all inputs( (2*m*x*x) + (2*x*x*c) - (2*x*y)) ]
+     *
+     */
+    int n = input_x.size();
+    float slope = 0;
+
+    for(int iter = 0; iter < n; iter++) {
+        slope +=    (2 * m * input_x[iter] * input_x[iter]) +
+                    (2 * input_x[iter] * c) -
+                    (2 * input_x[iter] * input_y[iter]);        
+    } 
+    return (slope * ((float)1 / (float)(2 * n)))
+}
+
+float cost_function_c_slope(vector<float> input_x,
+                            vector<float> input_y,
+                            float m,
+                            float c) {
+    assert(input_x.size() == input_y.size());
+
+    /*
+     * Note : n is the number of input points
+     * Cost function : [ (1/2n) * summation over all inputs((2*c) + (2*m*x) - (2*y)) ]
+     *
+     */
+    int n = input_x.size();
+    float slope = 0;
+
+    for(int iter = 0; iter < n; iter++) {
+        slope +=    (2 * c) +
+                    (2 * m * input_x[iter]) - 
+                    (2 * input_y[iter]);
+    } 
+    return (slope * ((float)1 / (float)(2 * n)))
+}
+
+weights perform_linear_regression_2d(  lr_2d_input input,
+                                       weights init_weights = weights()) {
+    assert(input.input_x.size() == input.input_y.size());
+    assert(input.input_x.size() > 0);
+    /* Randomly init weights */
+    weights w = init_weights;
+    
+    int iter = 0;
+    while(iter < input.epoch) {
+        m_slope = cost_function_m_slope(input.input_x, input.input_y, w.m, w.c) * input.learning_rate;
+        c_slope = cost_function_c_slope(input.input_x, input.input_y, w.m, w.c) * input.learning_rate;
+        w.m -= m_slope;
+        w.c -= c_slope;  
+        iter++;
+    }
+    
+    return w;
+}
---
+++ linear_regression/linear_regression_2d/lr_2d.hpp
@@ -0,0 +1,60 @@
+#ifndef __LR_2D_H__
+#define __LR_2D_H__
+
+/* This file declares the headers to use
+ * linear regression 2D code;
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef struct weights {
+    /* Hypothesis is h = mx + c */
+    float m; // weight 0
+    float c; // weight 1
+
+    weights() {
+        /* Random */
+        m = 5;
+        c = 2; 
+    }
+    weights(float m_, float c_) {
+        m = m_;
+        c = c_;
+    }
+}weights; 
+
+typedef struct lr_2d_input {
+    /* 2d input points */
+    vector<float> input_x;
+    vector<float> input_y;
+
+    /* Learning parameters */
+    float learning_rate;
+    int epoch;
+    
+    lr_2d_input() {
+        /* def values */
+        learning_rate = 0.2;
+        epoch = 1000;
+    }
+    
+    lr_2d_input(vector<float> input_x_, vector<float> input_y_, float learning_rate_, int epoch_) {
+        input_x = input_x_;
+        input_y = input_y_;
+        learning_rate = learning_rate_;
+        epoch = epoch_;
+    }
+
+    lr_2d_input(vector<float> input_x_, vector<float> input_y_) {
+        input_x = input_x_;
+        input_y = input_y_;
+        learning_rate = 0.2;
+        epoch = 1000;
+    }
+}lr_2d_input;
+
+weights perform_linear_regression_2d(lr_2d_input input, weights init_weights = weights());
+
+#endif // __LR_2D_H__
