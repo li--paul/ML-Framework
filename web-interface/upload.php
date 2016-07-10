5cc65dadcf0c704c67ba74d6fe4b537d4a965113
Varun Sundar <varunsundar08@gmail.com>
2016-07-10 21:19:29 +0530

    [web-dev] Save uploaded csv with a uniq id

--- web-interface/upload.php
+++ web-interface/upload.php
@@ -4,13 +4,21 @@
     ini_set('upload_max_filesize', '1000M');
     ini_set('post_max_size', '1500M');
 
+    $upload_file = basename($_FILES["fileinput"]["name"]);
+    $upload_file_parts = explode('.', $upload_file);
+    if(end($upload_file_parts) != "csv") {
+        echo $upload_file . "Not a csv file";
+        return;
+    }
+
+    $uniq_id = $_POST['uniq_id'];
     $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileinput"]["name"]);
+    $target_file = $target_dir . $uniq_id . '.csv'; 
 
     if (move_uploaded_file($_FILES["fileinput"]["tmp_name"], $target_file)) {
         echo "Upload success.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "Upload failed.";
     }
 
     session_destroy();
--- web-interface/util_functions.js
+++ web-interface/util_functions.js
@@ -1,14 +1,31 @@
+var global_uniq_id="";
 $("#uploadForm").submit(function(event) {
     event.preventDefault();
-    var fd = new FormData(document.getElementById('uploadForm'));
+
+    // Get unique id
     $.ajax({
-        url: "upload.php",
-        type: "POST",
-        data: fd,
-        contentType: false,
-        processData: false,
-        success: function(str) {
-            alert(str);
+        url: "get_unique_id.php",
+        type: "GET",
+        success: function(uniq_id) {
+            // Try to upload file
+            global_uniq_id = uniq_id;
+
+            var fd = new FormData(document.getElementById('uploadForm'));
+            fd.append("uniq_id", uniq_id);
+            $.ajax({
+                url: "upload.php",
+                type: "POST",
+                data: fd,
+                contentType: false,
+                processData: false,
+                success: function(str) {
+                    if(str.localeCompare("Upload success.") == 0) {
+                        alert("Upload success... Processing");
+                    } else {
+                        alert(str + "... Aborting");
+                    }
+                }
+            });
         }
     });
 });
