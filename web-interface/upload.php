8cef2754970bc97b41cf2cc6d7111135821c55d8
Varun Sundar <varunsundar08@gmail.com>
2016-07-10 19:23:56 +0530

    [web-dev] Add fileupload code

---
+++ web-interface/upload.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    ini_set('file_uploads', 'On');
+    ini_set('upload_max_filesize', '1000M');
+    ini_set('post_max_size', '1500M');
+
+    echo $_FILES['fileinput']['name'] . " ----- " . $_FILES['fileinput']['tmp_name'];
+
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileinput"]["name"]);
+
+    if (move_uploaded_file($_FILES["fileinput"]["tmp_name"], $target_file)) {
+        echo "Upload success.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+
+    session_destroy();
+?>
---
+++ web-interface/util_functions.js
@@ -0,0 +1,28 @@
+$("#uploadForm").submit(function(event) {
+    event.preventDefault();
+    var fd = new FormData(document.getElementById('uploadForm'));
+    $.ajax({
+        url: "upload.php",
+        type: "POST",
+        data: fd,
+        contentType: false,
+        processData: false,
+        success: function(str) {
+            alert(str);
+        }
+    });
+});
+
+function selectFile() {
+    var fi = document.getElementById('fileinput');
+    fi.click();
+} 
+
+function reflectChange() {
+    document.getElementById('filename').value = document.getElementById('fileinput').value;
+}
+
+
+function uploadFile() {
+    return false;
+}
