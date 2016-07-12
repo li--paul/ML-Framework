279b839af2d75d72d17c180250a0d2dc22966fd8
Varun Sundar <varunsundar08@gmail.com>
2016-07-13 00:30:37 +0530

    [web-dev] Comment return codes

--- web-interface/upload.php
+++ web-interface/upload.php
@@ -1,10 +1,20 @@
 <?php
+
+    /*
+     * The script echos the following error codes
+     * 000 - Input file upload success && Weights file upload success (Continue processing)
+     * 1XX - Input file upload failed && Weights file upload not attempted (Abort)
+     * 01X - Input file upload success && Weights file does not make sense (Continue)
+     * 001 - Input file upload success && Weights file not uploaded (Abort)
+     */
+    
     session_start();
     ini_set('file_uploads', 'On');
     ini_set('upload_max_filesize', '1000M');
     ini_set('post_max_size', '1500M');
 
-    $upload_file = basename($_FILES["fileinput"]["name"]);
+    /* Upload input file */
+    $upload_file = basename($_FILES["fileinput_input"]["name"]);
     $upload_file_parts = explode('.', $upload_file);
     if(end($upload_file_parts) != "csv") {
         echo $upload_file . "Not a csv file";
@@ -15,7 +25,7 @@
     $target_dir = "uploads/";
     $target_file = $target_dir . $uniq_id . '.csv'; 
 
-    if (move_uploaded_file($_FILES["fileinput"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["fileinput_input"]["tmp_name"], $target_file)) {
         echo "Upload success.";
     } else {
         echo "Upload failed.";
